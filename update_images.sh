36e6e50620775af5e81a005737b8c087a7ec15c7
BBerastegui <borjaberastegui@gmail.com>
2019-02-06 14:06:22 +0000

    Fixed bash script

--- update_images.sh
+++ update_images.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ $(cat ~/.docker/config.json | jq -r '.auths') = "{}" ]
+then
+    echo "[i] Login to a docker repo first.";
+    exit 1;
+fi
+
 for image in *
 do
     if [ -d "${image}" ]; then
       echo "[i] Rebuilding bberastegui/${image} ..."
-      docker build --no-cache ./${image} 
+      docker build --no-cache -t bberastegui/${image} ./${image}
       docker push bberastegui/${image}
     fi
 done
