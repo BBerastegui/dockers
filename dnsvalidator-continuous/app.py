7a2985773c252f08ac2bf544195fbca0046b4b25
BBerastegui <bberastegui@debian7>
2019-10-30 10:09:43 -0700

    Flask

--- dnsvalidator-continuous/app.py
+++ dnsvalidator-continuous/app.py
@@ -1,16 +1,23 @@
 import os
 
 from flask import Flask
+from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/refresh-dns')
 def hello_world():
+    thread = Thread(target=threaded_task)
+    thread.daemon = True
+    thread.start()
+    return 'OK'
+
+def threaded_task():
     bashCommand = "/bin/sh /entrypoint.sh"
     import subprocess
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
-    return 'OK'
+    return
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
